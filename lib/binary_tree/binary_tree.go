f17d1216c13fee75a7d10dfb20b0c3f86fe259d3
Bryan Goldstein <bryan@pivotallabs.com>
2013-11-04 22:25:18 -0500

    Add insert function to binary search tree

--- lib/binary_tree/binary_tree.go
+++ lib/binary_tree/binary_tree.go
@@ -1,6 +1,7 @@
-package gofigure
+package bt
 
 type AnyType interface{}
+type Comparer func(one, two AnyType) (bool)
 
 type Tree struct {
   Left  *Tree
@@ -24,5 +25,24 @@ func (parent *Tree) InsertRight(value AnyType) (child *Tree) {
   child = New()
   child.Value = value
   parent.Right = child
+  return
+}
+
+func (parent *Tree) Insert(value AnyType, lessThan Comparer) (child *Tree) {
+  
+  if lessThan(value, parent.Value) {
+    if parent.Left == nil {
+      child = parent.InsertLeft(value)
+    } else {
+      child = parent.Left.Insert(value, lessThan)
+    }
+  } else {
+    if parent.Right == nil {
+      child = parent.InsertRight(value)
+    } else {
+      child = parent.Right.Insert(value, lessThan)
+    }
+  }
+
   return
 }
--- spec/binary_tree_test.go
+++ spec/binary_tree_test.go
@@ -1,7 +1,7 @@
 package gofigure_test
 
 import (
-  bt "../lib/binary_tree"
+  "../lib/binary_tree"
   . "github.com/onsi/ginkgo"
   . "github.com/onsi/gomega"
 )
@@ -36,6 +36,27 @@ func init() {
       Expect(right_node).To(Equal(node.Right))
       Expect(right_node.Value).To(Equal("hello"))
     })
+
+  })
+
+  It("inserts values in the right order", func() {
+      var t *bt.Tree = bt.New()
+
+      t.Value = 5
+
+      compare := bt.Comparer(func(one, two bt.AnyType) (bool) {
+        return one.(int) < two.(int)
+      })
+
+      node := t.Insert(3, compare)
+
+      Expect(node).To(Equal(t.Left))
+      Expect(node.Value).To(Equal(3))
+
+      right_node := node.Insert(4, compare)
+
+      Expect(right_node).To(Equal(node.Right))
+      Expect(right_node.Value).To(Equal(4))
 
   })
 }
