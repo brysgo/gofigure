14bb53b7b6a451d535c31309b3f788a548cd0312
Bryan Goldstein <bryan@pivotallabs.com>
2013-11-07 00:40:53 -0500

    Implement naive decision tree training algorithm

--- lib/decision_tree/decision_tree.go
+++ lib/decision_tree/decision_tree.go
@@ -1,7 +1,48 @@
 package gofigure
 
 import (
-  // bt "../binary_tree"
+  bt "../binary_tree"
 )
 
+type DecisionTree struct {
+  bt.Tree
+}
+type Input map[string]bool
+type InputLog []Input
+
+func (self *DecisionTree) Train(inputLog InputLog, targetKey string) {
+  nextKey := inputLog.maxEntropy(targetKey)
+  if nextKey != targetKey {
+    leftInputLog, rightInputLog := inputLog.splitOnKey(nextKey)
+    left, right := new(DecisionTree), new(DecisionTree)
+    left.Train(leftInputLog, targetKey)
+    right.Train(rightInputLog, targetKey)
+    self.Left, self.Right = &left.Tree, &right.Tree
+  } else {
+    self.Value = inputLog[0][targetKey]
+  }
+}
+
+func (self InputLog) maxEntropy(targetKey string) string {
+  var key string
+  for key, _ = range self[0] { break }
+  return key
+}
+
+func (self InputLog) splitOnKey(targetKey string) (left InputLog, right InputLog) {
+  for _, v := range self {
+    remainder := make(Input, len(v)-1)
+    for k, b := range v {
+      if k != targetKey {
+        remainder[k]=b
+      }
+    }
+    if v[targetKey] {
+      right = append(right, remainder)
+    } else {
+      left = append(left, remainder)
+    }
+  }
+  return
+}
 
--- spec/decision_tree_test.go
+++ spec/decision_tree_test.go
@@ -1,16 +1,31 @@
 package gofigure_test
 
 import (
-  // bt "../lib/decision_tree"
+  dt "../lib/decision_tree"
   . "github.com/onsi/ginkgo"
-  // . "github.com/onsi/gomega"
+  . "github.com/onsi/gomega"
 )
 
 func init() {
   var _ = Describe("Decision Tree", func() {
 
-    It("", func() {
+    It("builds a decision tree from a list of input features and an output feature", func() {
+      xorInputSet := dt.InputLog{
+        {"0":true, "1":false, "2":true},
+        {"0":false,"1":false,"2":false},
+        {"0":true, "1":true, "2":false},
+        {"0":false, "1":true, "2":true},
+      }
+
+      myTree := new(dt.DecisionTree)
+      myTree.Train(xorInputSet, "2")
+
+      Expect(myTree.Left).ToNot(BeNil())
+      Expect(myTree.Left.Right).ToNot(BeNil())
+      Expect(myTree.Left.Right.Value).To(Equal(true))
     })
+
+    XIt("it uses ID3 to maximize entropy", func(){})
 
   })
 }
